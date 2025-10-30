8b81199fc0f2707a8fcf2bec3beb09e2f23b01d4
beeftoad2 <johnsonc30@wit.edu>
2025-10-30 08:50:40 -0400

    Completed Part A

--- parta.c
+++ parta.c
@@ -1,8 +1,30 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>  
+#include <ctype.h>  
 
-// TODO: Include more header files (read the manpages to find out more!)
-
+/**
+ * Prints command line arguments with all alphabet letters capitalized,
+ * separated by commas. If no arguments are provided, prints an error message.
+ */
 int main(int argc, const char* argv[]) {
+    if (argc <= 1) {
+        printf("ERROR: No arguments\n");
+        return 1;
+    }
+    
+    for (int i = 1; i < argc; i++) {
+        if (i > 1) {
+            printf(",");
+        }
+        
+        const char* arg = argv[i];
+        for (int j = 0; arg[j] != '\0'; j++) {
+            printf("%c", toupper(arg[j]));
+        }
+    }
+    
+    printf("\n");
+    
     return 0;
 }
